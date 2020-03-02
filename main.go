fa0d3136095282d574be7f9118193deea7b56fd4
JulienCHATEAU <jujugoal44@hotmail.fr>
2020-03-02 17:25:54 +0100

    Changing map has been fixed

--- main.go
+++ main.go
@@ -17,17 +17,45 @@ var WINDOW_BCK rl.Color = rl.NewColor(245, 239, 220, 255) // Light Beige
 var BORDER_COLOR rl.Color = rl.Gold
 
 func getOpositeOri(ori ym.Orientation) ym.Orientation {
-	if ori == ym.NORTH {
-		return ym.SOUTH
-	} else if ori == ym.SOUTH {
-		return ym.NORTH
-	} else if ori == ym.WEST {
-		return ym.EAST
-	} else if ori == ym.EAST {
-		return ym.WEST
-	} else {
-		return ym.NONE
+	var oppositeOri ym.Orientation
+	switch ori {
+		case ym.NORTH:
+			oppositeOri = ym.SOUTH
+			break
+		case ym.SOUTH:
+			oppositeOri = ym.NORTH
+			break
+		case ym.WEST:
+			oppositeOri = ym.EAST
+			break
+		case ym.EAST:
+			oppositeOri = ym.WEST
+			break
+
+		default:
+			oppositeOri = ym.NONE
+			break
 	}
+	return oppositeOri
+}
+
+func getNextCoord(ori ym.Orientation, coord Coord) Coord {
+	var newCoord Coord = coord
+	switch ori {
+		case ym.NORTH:
+			newCoord.Y++
+			break
+		case ym.SOUTH:
+			newCoord.Y--
+			break
+		case ym.WEST:
+			newCoord.X--
+			break
+		case ym.EAST:
+			newCoord.X++
+			break
+	}
+	return newCoord
 }
 
 func generateOri(remainingMapCount, notCreatedYet *int, oppositeOri ym.Orientation) []ym.Orientation {
@@ -67,31 +95,34 @@ func generateOri(remainingMapCount, notCreatedYet *int, oppositeOri ym.Orientati
 	return opening
 }
 
+type Coord struct {
+	X int32
+	Y int32
+}
+
 func main() {
 
 	var remainingMapCount int = 10
 	var notCreatedYet int = 1
-	var _maps []ym.Map = make([]ym.Map, 1)
-	var currentMapIndex = 0
-	_maps[currentMapIndex].Init(WINDOW_SIZE, []ym.Orientation{ym.NORTH})
-	_maps[currentMapIndex].CurrPlayer.Init(_maps[currentMapIndex].Width - 50, _maps[currentMapIndex].Height - 50, ym.NORTH)
+	var currentMapCoord Coord = Coord{0, 0}
+	var _maps map[Coord]*ym.Map = make(map[Coord]*ym.Map)
+	var _map *ym.Map = &ym.Map{}
+	_map.Init(WINDOW_SIZE, []ym.Orientation{ym.NORTH})
+	_map.CurrPlayer.Init(_map.Width - 50, _map.Height - 50, ym.NORTH)
+	_maps[currentMapCoord] = _map
 	remainingMapCount--
 
-	var mapsLink []map[ym.Orientation]int = make([]map[ym.Orientation]int, 1)
-	mapsLink[0] = make(map[ym.Orientation]int)
-	mapsLink[0][ym.NORTH] = -1
-	mapsLink[0][ym.SOUTH] = -1
-	mapsLink[0][ym.EAST] = -1
-	mapsLink[0][ym.WEST] = -1
-
 	fmt.Println("Ymaets")
-	rl.InitWindow(_maps[currentMapIndex].Width + MENU_SIZE, _maps[currentMapIndex].Height, "Ymaets")
+	rl.InitWindow(_maps[currentMapCoord].Width + MENU_SIZE, _maps[currentMapCoord].Height, "Ymaets")
 	rl.HideCursor()
 	rl.SetTargetFPS(60)
 	var savedX int32
 	var savedY int32
 	var index int32 
 	var changeMapOri ym.Orientation
+	var newMapIndex Coord
+	var oppositeOri ym.Orientation
+	var opening []ym.Orientation
 
 	for !rl.WindowShouldClose() {
 
@@ -100,61 +131,51 @@ func main() {
 			mouseX := rl.GetMouseX()
 			mouseY := rl.GetMouseY()
 
-			_maps[currentMapIndex].WallsDraw()
+			_maps[currentMapCoord].WallsDraw()
 
-			for index = 0; index < _maps[currentMapIndex].ShotsCount; index++ {
-				_maps[currentMapIndex].Shots[index].Draw()
-				_maps[currentMapIndex].ShotMove(&index)
-				_maps[currentMapIndex].ShotCheckMoveCollision(&index)
+			for index = 0; index < _maps[currentMapCoord].ShotsCount; index++ {
+				_maps[currentMapCoord].Shots[index].Draw()
+				_maps[currentMapCoord].ShotMove(&index)
+				_maps[currentMapCoord].ShotCheckMoveCollision(&index)
 			}
 
 			
-			for index = 0; index < _maps[currentMapIndex].MonstersCount; index++ {
-				savedX = _maps[currentMapIndex].Monsters[index].X
-				savedY = _maps[currentMapIndex].Monsters[index].Y
-				_maps[currentMapIndex].Monsters[index].Draw()
-				_maps[currentMapIndex].MonsterMove(index)
-				_maps[currentMapIndex].MonsterCheckMoveCollision(&index, savedX, savedY)
+			for index = 0; index < _maps[currentMapCoord].MonstersCount; index++ {
+				savedX = _maps[currentMapCoord].Monsters[index].X
+				savedY = _maps[currentMapCoord].Monsters[index].Y
+				_maps[currentMapCoord].Monsters[index].Draw()
+				_maps[currentMapCoord].MonsterMove(index)
+				_maps[currentMapCoord].MonsterCheckMoveCollision(&index, savedX, savedY)
 			}
 
-			savedX = _maps[currentMapIndex].CurrPlayer.X
-			savedY = _maps[currentMapIndex].CurrPlayer.Y
-			savedOri := _maps[currentMapIndex].CurrPlayer.Ori
-			_maps[currentMapIndex].PlayerOri(mouseX, mouseY)
-			_maps[currentMapIndex].PlayerCheckOriCollision(savedOri)
-			changeMapOri = _maps[currentMapIndex].PlayerMove()
+			savedX = _maps[currentMapCoord].CurrPlayer.X
+			savedY = _maps[currentMapCoord].CurrPlayer.Y
+			savedOri := _maps[currentMapCoord].CurrPlayer.Ori
+			_maps[currentMapCoord].PlayerOri(mouseX, mouseY)
+			_maps[currentMapCoord].PlayerCheckOriCollision(savedOri)
+			changeMapOri = _maps[currentMapCoord].PlayerMove()
 			if changeMapOri == ym.NONE {
-				_maps[currentMapIndex].PlayerCheckMoveCollision(savedX, savedY)
-				_maps[currentMapIndex].PlayerFire()
-				_maps[currentMapIndex].PlayerDraw()
+				_maps[currentMapCoord].PlayerCheckMoveCollision(savedX, savedY)
+				_maps[currentMapCoord].PlayerFire()
+				_maps[currentMapCoord].PlayerDraw()
 			} else {
-				var newMapIndex int = mapsLink[currentMapIndex][changeMapOri]
-				if newMapIndex == -1 {
-					newMapIndex = len(_maps)
-					var _map ym.Map
-					var oppositeOri ym.Orientation = getOpositeOri(changeMapOri)
-					var opening []ym.Orientation = generateOri(&remainingMapCount, &notCreatedYet, oppositeOri)
-					_map.Init(WINDOW_SIZE, opening)
-					_map.MapChangeInit(_maps[currentMapIndex], changeMapOri, WINDOW_SIZE, opening)
-					_maps = append(_maps, _map)
-
-					mapsLink[currentMapIndex][changeMapOri] = newMapIndex
-					var newMapLink map[ym.Orientation]int = make(map[ym.Orientation]int)
-					newMapLink[ym.NORTH] = -1
-					newMapLink[ym.SOUTH] = -1
-					newMapLink[ym.EAST] = -1
-					newMapLink[ym.WEST] = -1
-					mapsLink = append(mapsLink, newMapLink)
-					mapsLink[newMapIndex][oppositeOri] = currentMapIndex
-					currentMapIndex = newMapIndex
-					} else {
-					_maps[newMapIndex].Update(_maps[currentMapIndex], changeMapOri, WINDOW_SIZE)
-					currentMapIndex = newMapIndex
+				newMapIndex = getNextCoord(changeMapOri, currentMapCoord)
+				if _, ok := _maps[newMapIndex]; ok {
+					_maps[newMapIndex].Update(*_maps[currentMapCoord], changeMapOri, WINDOW_SIZE)
+					currentMapCoord = newMapIndex
+				} else {
+					var nextMap *ym.Map = &ym.Map{}
+					oppositeOri  = getOpositeOri(changeMapOri)
+					opening = generateOri(&remainingMapCount, &notCreatedYet, oppositeOri)
+					nextMap.Init(WINDOW_SIZE, opening)
+					nextMap.MapChangeInit(*_maps[currentMapCoord], changeMapOri, WINDOW_SIZE, opening)
+					_maps[newMapIndex] = nextMap
+					currentMapCoord = newMapIndex
 				}
 			}
-			_maps[currentMapIndex].CursorMove(mouseX, mouseY)
-			_maps[currentMapIndex].CursorDraw()
-			_maps[currentMapIndex].DrawMenu(MENU_SIZE)
+			_maps[currentMapCoord].CursorMove(mouseX, mouseY)
+			_maps[currentMapCoord].CursorDraw()
+			_maps[currentMapCoord].DrawMenu(MENU_SIZE)
 		rl.EndDrawing()
 	}
 	fmt.Println(len(_maps))
